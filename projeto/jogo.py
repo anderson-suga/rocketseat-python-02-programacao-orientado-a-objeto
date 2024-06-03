f95c17d55f55f343e806cd98a32b60a89b0e616c
Anderson Suga <anderson.suga@gmail.com>
2024-06-03 20:59:57 -0300

    Criando o Orquestrador de Combate

--- projeto/jogo.py
+++ projeto/jogo.py
@@ -46,7 +46,25 @@ class Inimigo(Persnagem):
         return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}"
 
 
-heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
-print(heroi.exibir_detalhes())
-inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
-print(inimigo.exibir_detalhes())
+class Jogo:
+    """Classe orquestradora do jogo"""
+
+    def __init__(self) -> None:
+        self.heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
+        self.inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
+
+    def iniciar_batalha(self):
+        """Fazer a gestão da batalha em turnos"""
+        print("Iniciando a batalha!")
+        while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+            print("\nDetalhes dos personagens:")
+            print(f"{self.heroi.exibir_detalhes()}\n")
+            print(f"{self.inimigo.exibir_detalhes()}\n")
+
+            input("Pressione Enter para atacar...")
+            escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
+
+
+# Criar instância do jogo e iniciar batalha
+jogo = Jogo()
+jogo.iniciar_batalha()
